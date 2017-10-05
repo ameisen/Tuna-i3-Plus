bcafc0e9a93c175de28464b4617f18bf0ceae854
Michael <14104310+ameisen@users.noreply.github.com>
2017-10-05 14:06:37 -0500

    Build Cleanup
    Cleaning up the build a bit in MSVC.

--- Marlin/__vm/.Marlin.vsarduino.h
+++ Marlin/__vm/.Marlin.vsarduino.h
@@ -1,51 +1,30 @@
-/* 
-	Editor: http://www.visualmicro.com
-			visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
-			the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
-			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
-			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
-	
-	Hardware: Arduino/Genuino Mega w/ ATmega2560 (Mega 2560), Platform=avr, Package=arduino
-*/
+#if defined(__INTELLISENSE__)
 
-#if defined(_VMICRO_INTELLISENSE)
+#pragma once
 
-#ifndef _VSARDUINO_H_
-#define _VSARDUINO_H_
-#define __AVR_ATmega2560__
-#define _VMDEBUG 1
-#define F_CPU 16000000L
-#define ARDUINO 10801
-#define ARDUINO_AVR_MEGA2560
-#define ARDUINO_ARCH_AVR
-#define __cplusplus 201103L
-#define __AVR__
+#ifdef __cplusplus
+#	undef __cplusplus
+#endif
+#define __cplusplus 201703L
 #define __inline__
 #define __asm__(...)
 #define __extension__
-#define __inline__
-#define __volatile__
-#define GCC_VERSION 40902
+#define __volatile__(...)
 
-#define __cplusplus 201103L
-#undef __cplusplus
-#define __cplusplus 201103L
-
-#define volatile(va_arg) 
-#define _CONST
+#define volatile(...) 
 #define __builtin_va_start
 #define __builtin_va_end
 #define __attribute__(...)
-#define NOINLINE __attribute__((noinline))
+#define NOINLINE
 #define prog_void
-#define PGM_VOID_P int
+#define PGM_VOID_P void *
 
 
 #ifndef __builtin_constant_p
-	#define __builtin_constant_p __attribute__((__const__))
+	#define __builtin_constant_p
 #endif
 #ifndef __builtin_strlen
-	#define __builtin_strlen  __attribute__((__const__))
+	#define __builtin_strlen
 #endif
 
 
@@ -62,12 +41,12 @@ typedef void *__builtin_va_list;
 
 
 
-#include <arduino.h>
-#include <pins_arduino.h> 
+//#include <arduino.h>
+//#include <pins_arduino.h> 
 #undef F
-#define F(string_literal) ((const PROGMEM char *)(string_literal))
+#define F(string_literal) ((const char *)(string_literal))
 #undef PSTR
-#define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
+#define PSTR(string_literal) ((const char *)(string_literal))
 
 
 #define pgm_read_byte(address_short) uint8_t() 
@@ -76,6 +55,5 @@ typedef void *__builtin_va_list;
 #define pgm_read_float(address_short) float()
 #define pgm_read_ptr(address_short)   short()
 
-#include "Marlin.ino"
-#endif
+//#include "Marlin.ino"
 #endif
--- Marlin/tunalib/tunalib.hpp
+++ Marlin/tunalib/tunalib.hpp
@@ -1,3 +1,3 @@
 #pragma once
 
-#include "D:\Projects\i3PlusPlus\Marlin\tunalib/utils.hpp"
+#include "tunalib/utils.hpp"
